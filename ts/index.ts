253ba1da8ff6a5640a83087ea63eea4db0577d4f
Aakil Fernandes <aakilfernandes@gmail.com>
2020-03-15 11:44:31 -0400

    time units

--- ts/index.ts
+++ ts/index.ts
@@ -2,3 +2,11 @@ export const HUNDRED = 100
 export const THOUSAND = 1000
 export const MILLION = 1000000
 export const BILLION = 1000000000
+
+export const MINUTE_S = 60
+export const HOUR_S = MINUTE_S * 60
+export const DAY_S = HOUR_S * 24
+
+export const MINUTE_MS = MINUTE_S * THOUSAND
+export const HOUR_MS = HOUR_S * THOUSAND
+export const DAY_MS = DAY_S * THOUSAND
